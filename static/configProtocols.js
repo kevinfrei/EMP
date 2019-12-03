6d42b52f941cfd8e3c8cee42d7804eda2ae06f8d
Kevin Frei <kevinfrei@hotmail.com>
2019-12-02 21:20:18 -0800

    Updated for deprecated Electron API

--- static/configProtocols.js
+++ static/configProtocols.js
@@ -13,7 +13,7 @@ protocol.registerSchemesAsPrivileged([
 //logger.disable('configProtocols');
 const log = (...args: Array<mixed>) => logger('configProtocols', ...args);
 
-const songs = [
+const songs: string[] = [
   '09 - 7empest.flac',
   '02 - Pneuma.flac',
   '04 - Invincible.flac',
@@ -21,55 +21,36 @@ const songs = [
 ];
 
 const configureProtocols = () => {
-  protocol.registerFileProtocol(
-    'pic',
-    (req, callback) => {
-      log('pic URL request:');
-      log(req);
-      if (req.url.startsWith('pic://album/')) {
-        callback({
-          path:
-            '/Volumes/Thunderbolt/Audio/Sorted/Accurate/Tool - 2019 - Fear Inoculum/High/Cover.png'
-        });
-      } else {
-        callback({ path: path.join(__dirname, 'img-album.svg') });
-      }
-    },
-    error => {
-      if (error) {
-        log('failed to register "pic" protocol:');
-        log(error);
-      } else {
-        log('pic reg: did something, I guess');
-      }
+  protocol.registerFileProtocol('pic', (req, callback) => {
+    log('pic URL request:');
+    log(req);
+    if (!req.url) {
+      callback({ error: -324 });
+    } else if (req.url.startsWith('pic://album/')) {
+      callback({
+        path:
+          '/Volumes/Thunderbolt/Audio/Sorted/Accurate/Tool - 2019 - Fear Inoculum/High/Cover.png'
+      });
+    } else {
+      callback({ path: path.join(__dirname, 'img-album.svg') });
     }
-  );
-  protocol.registerFileProtocol(
-    'tune',
-    (req, callback) => {
-      log('tune URL request:');
-      log(req);
-      // req.url: "tune://song/song.flac"
-      if (req.url.startsWith('tune://song/')) {
-        const val = req.url.charCodeAt(12) % songs.length;
-        const thePath =
-          '/Volumes/Thunderbolt/Audio/Sorted/Accurate/Tool - 2019 - Fear Inoculum/High/' +
-          songs[val];
-        log('Returning path ' + thePath);
-        callback({ path: thePath });
-      } else {
-        // Do something?
-      }
-    },
-    error => {
-      if (error) {
-        log('failed to register "tune" protocol:');
-        log(error);
-      } else {
-        log('tune reg: did something I guess');
-      }
+  });
+  protocol.registerFileProtocol('tune', (req, callback) => {
+    log('tune URL request:');
+    log(req);
+    if (!req.url) {
+      callback({ error: -324 });
+    } else if (req.url.startsWith('tune://song/')) {
+      const val = req.url.charCodeAt(12) % songs.length;
+      const thePath =
+        '/Volumes/Thunderbolt/Audio/Sorted/Accurate/Tool - 2019 - Fear Inoculum/High/' +
+        songs[val];
+      log('Returning path ' + thePath);
+      callback({ path: thePath });
+    } else {
+      callback({ error: 404 });
     }
-  );
+  });
 };
 
 module.exports = configureProtocols;
