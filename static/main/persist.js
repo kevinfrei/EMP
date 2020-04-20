3fb29f6abccb0bbdc62efad58ccac468aa2044ca
Kevin Frei <kevinfrei@hotmail.com>
2020-04-20 12:01:24 -0700

    Fixed a bug in subscribe value. Added async funcs to persist.

--- static/main/persist.js
+++ static/main/persist.js
@@ -3,6 +3,7 @@
 const { app } = require('electron');
 const path = require('path');
 const fs = require('fs');
+const fsp = fs.promises;
 const logger = require('simplelogger');
 const { FTON, SeqNum } = require('my-utils');
 
@@ -10,7 +11,7 @@ import type { Rectangle } from 'electron';
 import type { FTONData } from 'my-utils';
 
 const log = logger.bind('persist');
-logger.disable('persist');
+//logger.enable('persist');
 
 export type MaybeRectangle = {|
   width: number,
@@ -37,10 +38,6 @@ export type Persist = {
   unsubscribe(id: string): boolean,
 };
 
-const memoryCache: Map<string, FTONData> = new Map();
-const listeners: Map<string, Map<string, ValueUpdateListener>> = new Map();
-const getNextListenerId = SeqNum();
-
 const KeyWhiteList = (name: string): boolean => {
   switch (name) {
     case 'nowPlaying':
@@ -53,21 +50,9 @@ const KeyWhiteList = (name: string): boolean => {
   }
 };
 
-const makeWindowPos = (
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-  isMaximized: boolean
-) => ({ bounds: { x, y, width, height }, isMaximized });
-
-const defaultWindowPosition: WindowPosition = makeWindowPos(
-  Number.MIN_SAFE_INTEGER,
-  Number.MIN_SAFE_INTEGER,
-  900,
-  680,
-  false
-);
+const memoryCache: Map<string, FTONData> = new Map();
+const listeners: Map<string, Map<string, ValueUpdateListener>> = new Map();
+const getNextListenerId = SeqNum();
 
 // Here's a place for app settings & stuff...
 function storageLocation(id: string): string {
@@ -89,14 +74,39 @@ function readFile(id: string): FTONData {
   return {};
 }
 
+async function readFileAsync(id: string): Promise<FTONData> {
+  let data = memoryCache.get(id);
+  if (data) {
+    return data;
+  }
+  try {
+    const contents = await fsp.readFile(storageLocation(id), 'utf8');
+    data = FTON.parse(contents);
+    return data;
+  } catch (e) {}
+  return {};
+}
+
 function writeFile(id: string, val: FTONData): void {
   fs.writeFileSync(storageLocation(id), FTON.stringify(val), 'utf8');
+  memoryCache.set(id, val);
+}
+
+async function writeFileAsync(id: string, val: FTONData): Promise<void> {
+  await fsp.writeFile(storageLocation(id), FTON.stringify(val), 'utf8');
   memoryCache.set(id, val);
 }
 
 function deleteFile(id: string) {
   try {
     fs.unlinkSync(storageLocation(id));
+    memoryCache.delete(id);
+  } catch (e) {}
+}
+
+async function deleteFileAsync(id: string) {
+  try {
+    await fsp.unlink(storageLocation(id));
     memoryCache.delete(id);
   } catch (e) {}
 }
@@ -149,6 +159,21 @@ function getItem<T>(key: string): ?T {
   }
 }
 
+// Get a value from disk/memory
+async function getItemAsync<T>(key: string): ?T {
+  log('Reading ' + key);
+  const val: FTONData = await readFileAsync(key);
+  if (val && typeof val === 'object' && val.hasOwnProperty(key)) {
+    if (key.toLocaleLowerCase() !== 'db') {
+      log('returning this value:');
+      log(val[key]);
+    }
+    return val[key];
+  } else {
+    log('Failed to return value');
+  }
+}
+
 // Save a value to disk and cache it
 function setItem(key: string, value: FTONData): void {
   log(`Writing ${key}:`);
@@ -158,7 +183,23 @@ function setItem(key: string, value: FTONData): void {
     if (val.hasOwnProperty(key) || KeyWhiteList(key)) {
       val[key] = value;
       writeFile(key, val);
-      notify(key, val);
+      notify(key, value);
+    } else {
+      log('Invalid item persistence request');
+    }
+  }
+}
+
+// Async Save a value to disk and cache it
+async function setItemAsync(key: string, value: FTONData): Promise<void> {
+  log(`Writing ${key}:`);
+  log(value);
+  const val: FTONData = await readFileAsync(key);
+  if (val && typeof val === 'object') {
+    if (val.hasOwnProperty(key) || KeyWhiteList(key)) {
+      val[key] = value;
+      await writeFileAsync(key, val);
+      notify(key, value);
     } else {
       log('Invalid item persistence request');
     }
@@ -170,6 +211,28 @@ function deleteItem(key: string): void {
   log(`deleting ${key}`);
   deleteFile(key);
 }
+
+// Async Delete an item (and remove it from the cache)
+async function deleteItemAsync(key: string): Promise<void> {
+  log(`deleting ${key}`);
+  await deleteFileAsync(key);
+}
+
+const makeWindowPos = (
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  isMaximized: boolean
+) => ({ bounds: { x, y, width, height }, isMaximized });
+
+const defaultWindowPosition: WindowPosition = makeWindowPos(
+  Number.MIN_SAFE_INTEGER,
+  Number.MIN_SAFE_INTEGER,
+  900,
+  680,
+  false
+);
 
 function getWindowPos(): WindowPosition {
   try {
@@ -211,8 +274,8 @@ function getBrowserWindowPos(st: WindowPosition): Rectangle {
   return {
     width: st.bounds.width,
     height: st.bounds.height,
-    x: st.bounds.x == Number.MIN_SAFE_INTEGER ? undefined : st.bounds.x,
-    y: st.bounds.y == Number.MIN_SAFE_INTEGER ? undefined : st.bounds.y,
+    x: (st.bounds.x == Number.MIN_SAFE_INTEGER) ? undefined : st.bounds.x,
+    y: (st.bounds.y == Number.MIN_SAFE_INTEGER) ? undefined : st.bounds.y,
   };
 }
 
@@ -220,6 +283,9 @@ const persist: Persist = {
   getItem,
   setItem,
   deleteItem,
+  getItemAsync,
+  setItemAsync,
+  deleteItemAsync,
   getWindowPos,
   setWindowPos,
   getBrowserWindowPos,
