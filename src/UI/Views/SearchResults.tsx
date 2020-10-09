b160493de4937f9453c019a1eaebcb6d388a6c31
Kevin Frei <kevinfrei@hotmail.com>
2020-10-08 20:15:28 -0700

    Slow progress toward usable search

--- src/UI/Views/SearchResults.tsx
+++ src/UI/Views/SearchResults.tsx
@@ -1,20 +1,153 @@
 import { Stack, Text } from '@fluentui/react';
 import { Logger } from '@freik/core-utils';
+import {
+  Album,
+  AlbumKey,
+  Artist,
+  ArtistKey,
+  Song,
+  SongKey,
+} from '@freik/media-utils';
 import React from 'react'; // eslint-disable-line @typescript-eslint/no-use-before-define
 import { useRecoilValue } from 'recoil';
-import { searchSel, searchTermAtom } from '../../Recoil/ReadOnly';
+import {
+  albumByKeySel,
+  allAlbumsSel,
+  allArtistsSel,
+  allSongsSel,
+  artistByKeySel,
+  searchSel,
+  searchTermAtom,
+  songByKeySel,
+} from '../../Recoil/ReadOnly';
+import { AlbumFromSong, ArtistsFromAlbum, ArtistsFromSong } from '../SongList';
 import { ViewProps } from './Selector';
+import './styles/SearchResults.css';
 
 const log = Logger.bind('SearchResults');
 // Logger.enable('SearchResults');
 
+function SongItem({ id }: { id: SongKey }): JSX.Element {
+  const song = useRecoilValue(songByKeySel(id));
+  return (
+    <span>
+      <span>{song.title}</span>
+      {ArtistsFromSong(song)}
+      {AlbumFromSong(song)}
+    </span>
+  );
+}
+
+function SongList({ songs }: { songs: SongKey[] }): JSX.Element {
+  return (
+    <>
+      {songs.map((val, index) => (
+        <SongItem key={index} id={val} />
+      ))}
+    </>
+  );
+}
+
+function AlbumItem({ id }: { id: AlbumKey }): JSX.Element {
+  const album = useRecoilValue(albumByKeySel(id));
+  return (
+    <span>
+      <span>
+        {album.title}-{album.year}
+      </span>
+      <span>{ArtistsFromAlbum(album)}</span>
+    </span>
+  );
+}
+
+function AlbumList({ albums }: { albums: AlbumKey[] }): JSX.Element {
+  return (
+    <>
+      {albums.map((val, index) => (
+        <AlbumItem key={index} id={val} />
+      ))}
+    </>
+  );
+}
+
+function ArtistItem({ id }: { id: ArtistKey }): JSX.Element {
+  const artist = useRecoilValue(artistByKeySel(id));
+  return (
+    <span>
+      {artist.name} - {artist.albums.length}
+    </span>
+  );
+}
+
+function ArtistList({ artists }: { artists: ArtistKey[] }): JSX.Element {
+  return (
+    <>
+      {artists.map((val, index) => (
+        <ArtistItem key={index} id={val} />
+      ))}
+    </>
+  );
+}
+
+type SongEntry = { key: SongKey; song: Song };
+type ArtistEntry = { key: ArtistKey; artist: Artist };
+type AlbumEntry = { key: AlbumKey; album: Album };
+type SearchEntry = SongEntry | AlbumEntry | ArtistEntry;
+
+function SongEntries(keys: SongKey[], songs: Map<SongKey, Song>): SongEntry[] {
+  return keys.map((v) => ({ key: v, song: songs.get(v)! }));
+}
+
+function ArtistEntries(
+  keys: ArtistKey[],
+  artists: Map<ArtistKey, Artist>,
+): ArtistEntry[] {
+  return keys.map((v) => ({ key: v, artist: artists.get(v)! }));
+}
+
+function AlbumEntries(
+  keys: AlbumKey[],
+  albums: Map<AlbumKey, Album>,
+): AlbumEntry[] {
+  return keys.map((v) => ({ key: v, album: albums.get(v)! }));
+}
+
 export default function SearchResultsView({ hidden }: ViewProps): JSX.Element {
   const searchTerm = useRecoilValue(searchTermAtom);
-  log('Searching:' + searchTerm);
   const searchResults = useRecoilValue(searchSel(searchTerm));
-  const songText = searchResults.songs.join(';');
-  const albumText = searchResults.albums.join(':');
-  const artistText = searchResults.artists.join('/');
+  const songs = useRecoilValue(allSongsSel);
+  const artists = useRecoilValue(allArtistsSel);
+  const albums = useRecoilValue(allAlbumsSel);
+
+  if (
+    !searchResults.albums.length &&
+    !searchResults.songs.length &&
+    !searchResults.artists.length
+  ) {
+    return (
+      <div
+        style={hidden ? { visibility: 'hidden' } : {}}
+        className="current-view empty-results"
+      >
+        <Text>No results found</Text>
+      </div>
+    );
+  }
+
+  const songRes = SongEntries(searchResults.songs, songs);
+  const artRes = ArtistEntries(searchResults.artists, artists);
+  const albRes = AlbumEntries(searchResults.albums, albums);
+  const resultEntries: SearchEntry[] = [...songRes, ...artRes, ...albRes];
+  // I need to create a group hierarchy like this:
+  // Songs => Song Results
+  // Artists => Artist List => Albums => Per Album Song Results
+  // Albums => Album List => Per Album Song Results
+  // To deal with that, I need unique keys for duplicate songs
+  // the key should be : s<song>, r<artist><song>, l<album><song>
+  // Sorting can't mess with the top level groups
+  // We should start with Songs, Artist, and Albums expanded
+  // which corresponds to the search results
+
   return (
     <div
       style={hidden ? { visibility: 'hidden' } : {}}
@@ -22,11 +155,11 @@ export default function SearchResultsView({ hidden }: ViewProps): JSX.Element {
     >
       <Stack>
         <Text>Songs</Text>
-        <Text>{songText}</Text>
+        <SongList songs={searchResults.songs} />
         <Text>Albums</Text>
-        <Text>{albumText}</Text>
+        <AlbumList albums={searchResults.albums} />
         <Text>Artist</Text>
-        <Text>{artistText}</Text>
+        <ArtistList artists={searchResults.artists} />
       </Stack>
     </div>
   );
