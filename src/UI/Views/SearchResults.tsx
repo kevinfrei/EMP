11fd044d5c1a59e43301b2a21386e775c337a767
Kevin Frei <kevinfrei@hotmail.com>
2022-06-18 09:33:37 -0700

    Upgrade deps, and a comment typo

--- src/UI/Views/SearchResults.tsx
+++ src/UI/Views/SearchResults.tsx
@@ -67,7 +67,7 @@ function GetArtistAlbum(groupKey: string): [ArtistKey, AlbumKey] | void {
   if (groupKey.startsWith('R*')) {
     const index = groupKey.lastIndexOf('*');
     if (index > 2) {
-      return [groupKey.substringing(2, 2 + index), groupKey.substring(index + 1)];
+      return [groupKey.substring(2, 2 + index), groupKey.substring(index + 1)];
     }
   }
 }
