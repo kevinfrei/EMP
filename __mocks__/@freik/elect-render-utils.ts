06f72cf82a7d4f874b57bfb407a9c4fda2b584a9
Kevin Frei <kevinfrei@hotmail.com>
2023-02-09 17:26:36 -0800

    nonworking testing

---
+++ __mocks__/@freik/elect-render-utils.ts
@@ -0,0 +1,14 @@
+export const Effects = {
+  syncWithMain: () => () => {
+    // Do something here?
+  },
+  oneWayFromMain: () => () => {
+    // And maybe here?
+  },
+  bidirectionalSyncWithTranslate: () => () => {
+    // And what about here?
+  },
+};
+export const ipc = () => {
+  // And maybe in here too?
+};
--- src/UI/__tests__/testVolumeControl.tsx
+++ src/UI/__tests__/testVolumeControl.tsx
@@ -1,20 +1,22 @@
-import { act } from 'react-test-renderer';
+import { Suspense } from 'react';
+import { act, create } from 'react-test-renderer';
+import { RecoilRoot } from 'recoil';
 import { FluentInitIcons } from '../../FluentInit';
+import { VolumeControl } from '../VolumeControl';
 
 jest.mock('../../MyWindow');
-
-export {};
+jest.mock('@freik/elect-render-utils');
 
 it('renders without crashing', async () => {
   await act(async () => {
     FluentInitIcons();
-    /*    create(
+    create(
       <RecoilRoot>
         <Suspense fallback="">
           <VolumeControl />
         </Suspense>
       </RecoilRoot>,
-    );*/
+    );
     return new Promise((res) => res());
   });
 });
