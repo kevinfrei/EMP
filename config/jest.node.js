210848122a65a8541b9647e9cd99721f581a7006
Kevin Frei <kevinfrei@hotmail.com>
2023-06-14 09:16:25 -0700

    Updated to a CJS/ESM dual mode @freik/logger module for Jest

--- config/jest.node.js
+++ config/jest.node.js
@@ -105,7 +105,7 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: '../static',
+  rootDir: '../electron',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
